3c1c8234a5fea3fc6365470248881cf5765a16fa
kjdeepak <kjdeepak4work@gmail.com>
2022-05-04 22:31:34 +0530

    Implement facebook auth sign in functionality

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  googleClientID: '171478953492-jval7mqo4vdd1c7h0jcm8pmjq9akri8p.apps.googleusercontent.com'
+  googleClientID: '171478953492-jval7mqo4vdd1c7h0jcm8pmjq9akri8p.apps.googleusercontent.com',
+  fbClientID: 'c1482cda488b1b93e59a928277e74f78'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  googleClientID: '171478953492-jval7mqo4vdd1c7h0jcm8pmjq9akri8p.apps.googleusercontent.com'
+  googleClientID: '171478953492-jval7mqo4vdd1c7h0jcm8pmjq9akri8p.apps.googleusercontent.com',
+  fbClientID: 'c1482cda488b1b93e59a928277e74f78'
 };
 
